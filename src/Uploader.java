4f90381ca8936b3fc37bb87bc28f49dfd5ccbf3f
Francesco Marretta <francescomsg@gmail.com>
2015-03-29 21:00:12 +0200

    Upload progressbar precision increased

--- src/Uploader.java
+++ src/Uploader.java
@@ -28,135 +28,134 @@ public class Uploader {
 	private ProgressDialog progressDialog;
 
 	// Per gli screen parziali
-	public Uploader(Rectangle r, String ip, int port){
-                try {
-                        SocketChannel socketChannel = createChannel(ip, port);
-                        this.socketChannel = socketChannel;
-                        
-                        Rectangle screenRect = new Rectangle(0, 0, 0, 0);
-                        for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
-                                screenRect = screenRect.union(gd.getDefaultConfiguration().getBounds());
-                        }
-                        
-                        this.img = new Robot().createScreenCapture(screenRect).getSubimage(r.x, r.y, r.width, r.height);
-                        
-                        ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
-                        ImageIO.write(img, "png", outputArray);
-                        outputArray.flush();
-                        this.bytes = outputArray.toByteArray();
-                        outputArray.close();
-                } catch (AWTException | IOException ex) {
-                        ex.printStackTrace();
-                }
+	public Uploader(Rectangle r, String ip, int port) {
+		try {
+			SocketChannel socketChannel = createChannel(ip, port);
+			this.socketChannel = socketChannel;
+
+			Rectangle screenRect = new Rectangle(0, 0, 0, 0);
+			for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
+				screenRect = screenRect.union(gd.getDefaultConfiguration().getBounds());
+			}
+
+			this.img = new Robot().createScreenCapture(screenRect).getSubimage(r.x, r.y, r.width, r.height);
+
+			ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
+			ImageIO.write(img, "png", outputArray);
+			outputArray.flush();
+			this.bytes = outputArray.toByteArray();
+			outputArray.close();
+		} catch (AWTException | IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	// Per gli screen completi
-	public Uploader(BufferedImage bi, String ip, int port){
+	public Uploader(BufferedImage bi, String ip, int port) {
 
-                try {
-                        SocketChannel socketChannel = createChannel(ip, port);
-                        this.socketChannel = socketChannel;
-                        this.img = bi;
-                        
-                        ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
-                        ImageIO.write(img, "png", outputArray);
-                        outputArray.flush();
-                        this.bytes = outputArray.toByteArray();
-                        outputArray.close();
-                } catch (IOException ex) {
-                        ex.printStackTrace();
-                }
+		try {
+			SocketChannel socketChannel = createChannel(ip, port);
+			this.socketChannel = socketChannel;
+			this.img = bi;
+
+			ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
+			ImageIO.write(img, "png", outputArray);
+			outputArray.flush();
+			this.bytes = outputArray.toByteArray();
+			outputArray.close();
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	// Per i file
-	public Uploader(String filePath, String ip, int port){
+	public Uploader(String filePath, String ip, int port) {
 
 		SocketChannel socketChannel = createChannel(ip, port);
 		this.socketChannel = socketChannel;
 		this.filePath = filePath;
 	}
 
-	public boolean send(String pass, String type){
-                try {
-                        dos = new DataOutputStream(socketChannel.socket().getOutputStream());
-                        dis = new DataInputStream(socketChannel.socket().getInputStream());
-                        
-                        
-                        // socketChannel.socket().setSoTimeout(10000);
-                        
-                        // send auth
-                        System.out.println("[Uploader] Sending auth");
-                        dos.writeUTF(pass);
-                        System.out.println("[Uploader] Auth sent: " + pass);
-                        this.link = dis.readUTF();
-                        System.out.println("[Uploader] Auth reply: " + link);
-                        if (this.link.equals("OK")) {
-                                
-                                System.out.println("Sending type: " + type);
-                                dos.writeUTF(type);
-                                
-                                // Controllo e aspetto che il server abbia ricevuto il type
-                                // corretto
-                                if (dis.readUTF().equals(type)) {
-                                        
-                                        System.out.println("Il server riceve un: " + type);
-                                        
-                                        switch (type) {
-                                                
-                                                // image transfer
-                                                case "img":
-                                                        
-                                                        System.out.println("[Uploader] Uploading image...");
-                                                        
-                                                        dos.writeInt(bytes.length);
-                                                        dos.write(bytes, 0, bytes.length);
-                                                        dos.flush();
-                                                        
-                                                        break;
-                                                        
-                                                        // file or txt transfer
-                                                case "file":
-                                                case "txt":
-                                                        
-                                                        sendFile(filePath);
-                                                        progressDialog.setWait();
-                                                        break;
-                                                        
-                                                        // default case, hmm
-                                                default:
-                                                        
-                                                        break;
-                                        }
-                                        
-                                        // return link
-                                        System.out.println("[Uploader] Waiting link...");
-                                        this.link = dis.readUTF();
-                                        System.out.println("[Uploader] Returned link: " + link);
-                                        if (type.equals("file"))
-                                                progressDialog.close();
-                                        bytes = null;
-                                } else {
-                                        System.out.println("[Uploader] The server had a bad interpretation of the fileType");
-                                        return false;
-                                }
-                                
-                        } else {
-                                System.out.println("[Uploader] Wrong password, closed");
-                                new NotificationDialog().wrongPassword();
-                                return false;
-                        }
-                        
-                        dos.close();
-                        dis.close();
-                        socketChannel.close();
-                        
-                } catch (Exception e) {
-                        e.printStackTrace();
-                        return false;
-                }
-                
-                return true;
-        }
+	public boolean send(String pass, String type) {
+		try {
+			dos = new DataOutputStream(socketChannel.socket().getOutputStream());
+			dis = new DataInputStream(socketChannel.socket().getInputStream());
+
+			// socketChannel.socket().setSoTimeout(10000);
+
+			// send auth
+			System.out.println("[Uploader] Sending auth");
+			dos.writeUTF(pass);
+			System.out.println("[Uploader] Auth sent: " + pass);
+			this.link = dis.readUTF();
+			System.out.println("[Uploader] Auth reply: " + link);
+			if (this.link.equals("OK")) {
+
+				System.out.println("Sending type: " + type);
+				dos.writeUTF(type);
+
+				// Controllo e aspetto che il server abbia ricevuto il type
+				// corretto
+				if (dis.readUTF().equals(type)) {
+
+					System.out.println("Il server riceve un: " + type);
+
+					switch (type) {
+
+					// image transfer
+					case "img":
+
+						System.out.println("[Uploader] Uploading image...");
+
+						dos.writeInt(bytes.length);
+						dos.write(bytes, 0, bytes.length);
+						dos.flush();
+
+						break;
+
+					// file or txt transfer
+					case "file":
+					case "txt":
+
+						sendFile(filePath);
+						progressDialog.setWait();
+						break;
+
+					// default case, hmm
+					default:
+						// TODO
+						break;
+					}
+
+					// return link
+					System.out.println("[Uploader] Waiting link...");
+					this.link = dis.readUTF();
+					System.out.println("[Uploader] Returned link: " + link);
+					if (type.equals("file"))
+						progressDialog.close();
+					bytes = null;
+				} else {
+					System.out.println("[Uploader] The server had a bad interpretation of the fileType");
+					return false;
+				}
+
+			} else {
+				System.out.println("[Uploader] Wrong password, closed");
+				new NotificationDialog().wrongPassword();
+				return false;
+			}
+
+			dos.close();
+			dis.close();
+			socketChannel.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
+	}
 
 	public void sendFile(String fileName) {
 		RandomAccessFile aFile = null;
@@ -173,8 +172,9 @@ public class Uploader {
 			progressDialog.setMessage("Caricando...");
 
 			// send the file
+			long bfSize = Math.min(131072, fileLength); // 128kB buffer
 			while (bytesSent < fileLength) {
-				bytesSent += inChannel.transferTo(bytesSent, fileLength - bytesSent, socketChannel);
+				bytesSent += inChannel.transferTo(bytesSent, bfSize, socketChannel);
 
 				// To secure overflow
 				try {
@@ -212,7 +212,7 @@ public class Uploader {
 			e.printStackTrace();
 		}
 	}
-        
+
 	private SocketChannel createChannel(String ip, int port) {
 
 		SocketChannel socketChannel = null;
