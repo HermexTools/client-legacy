f749972eaf00540ba4672fd735238a2a2f8484c5
Francesco Marretta <francescomsg@gmail.com>
2015-04-07 17:59:11 +0200

    .

--- src/it/ksuploader/main/Uploader.java
+++ src/it/ksuploader/main/Uploader.java
@@ -4,13 +4,6 @@ import it.ksuploader.dialogs.NotificationDialog;
 import it.ksuploader.dialogs.ProgressDialog;
 import it.ksuploader.utils.Environment;
 
-import java.awt.AWTException;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Rectangle;
-import java.awt.Robot;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -22,71 +15,15 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
 import java.nio.channels.UnresolvedAddressException;
 import java.util.Arrays;
-import java.util.Random;
-
-import javax.imageio.ImageIO;
 
 public class Uploader {
-	private BufferedImage img;
-	private byte[] bytes;
 	private SocketChannel socketChannel;
 	private String link;
 	private String filePath;
 	private DataOutputStream dos;
 	private DataInputStream dis;
 	private ProgressDialog progressDialog;
-    private static NotificationDialog dialog = new NotificationDialog();
-
-	// Per gli screen parziali
-	public Uploader(Rectangle r) {
-		try {
-			SocketChannel socketChannel = createChannel(Main.config.getIp(), Main.config.getPort());
-			this.socketChannel = socketChannel;
-
-			Rectangle screenRect = new Rectangle(0, 0, 0, 0);
-			for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
-				screenRect = screenRect.union(gd.getDefaultConfiguration().getBounds());
-			}
-
-			this.img = new Robot().createScreenCapture(screenRect).getSubimage(r.x, r.y, r.width, r.height);
-
-			ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
-			ImageIO.write(img, "png", outputArray);
-			outputArray.flush();
-			this.bytes = outputArray.toByteArray();
-			outputArray.close();
-            if(Main.config.isSaveEnabled()){
-                ImageIO.write(img, "png", new File(Main.config.getSaveDir()+"/"+System.currentTimeMillis() / 1000 + "" + new Random().nextInt(999)+".png"));
-                Main.myLog("[Uploader] Screen saved");
-            }
-		} catch (AWTException | IOException ex) {
-			ex.printStackTrace();
-            Main.myErr(Arrays.toString(ex.getStackTrace()).replace(",", "\n"));
-		}
-	}
-
-	// Per gli screen completi
-	public Uploader(BufferedImage bi) {
-
-		try {
-			SocketChannel socketChannel = createChannel(Main.config.getIp(), Main.config.getPort());
-			this.socketChannel = socketChannel;
-			this.img = bi;
-
-			ByteArrayOutputStream outputArray = new ByteArrayOutputStream();
-			ImageIO.write(img, "png", outputArray);
-			outputArray.flush();
-			this.bytes = outputArray.toByteArray();
-			outputArray.close();
-            if(Main.config.isSaveEnabled()){
-                ImageIO.write(img, "png", new File(Main.config.getSaveDir()+"/"+System.currentTimeMillis() / 1000 + "" + new Random().nextInt(999)+".png"));
-                Main.myLog("[Uploader] Screen saved");
-            }
-		} catch (IOException ex) {
-			ex.printStackTrace();
-            Main.myLog(ex.getMessage());
-		}
-	}
+	private static NotificationDialog dialog = new NotificationDialog();
 
 	// Per i file
 	public Uploader(String filePath) {
@@ -109,75 +46,42 @@ public class Uploader {
 			Main.myLog("[Uploader] Auth sent: " + Main.config.getPass());
 			this.link = dis.readUTF();
 			Main.myLog("[Uploader] Auth reply: " + link);
-            
+
 			if (this.link.equals("OK")) {
 
 				Main.myLog("Sending type: " + type);
 				dos.writeUTF(type);
-                String srvResponse;
+				String srvResponse;
 				// Controllo e aspetto che il server abbia ricevuto il type
 				// corretto
-                if (dis.readUTF().equals(type)) {
-                    
-                    Main.myLog("Il server riceve un: " + type);
-                    
+				if (dis.readUTF().equals(type)) {
 
-                        
-                        switch (type) {
-                            
-                            // image transfer
-                            case "img":
-                                
-                                Main.myLog("[Uploader] Uploading image...");
-                                
-                                dos.writeInt(bytes.length);
-                                
-                                srvResponse = dis.readUTF();
-                                if(srvResponse.equals("START_TRANSFER")){
-                                    dos.write(bytes, 0, bytes.length);
-                                    dos.flush();
-                                } else if(srvResponse.equals("SERVER_FULL")){
-                                    Main.myLog("[Uploader] Server Full");
-                                    dialog.serverFull();
-                                }
-                                
-                                break;
-                                
-                                // file or txt transfer
-                            case "file":
-                            case "txt":
-                                
-                                File file = new File(filePath);
-                                long fileLength = file.length();
-                                Main.myLog("[Uploader] File length: " + fileLength);
-                                dos.writeLong(fileLength);
-                                srvResponse = dis.readUTF();
-                                if(srvResponse.equals("START_TRANSFER")){
-                                    sendFile(file,fileLength);
-                                }else if(srvResponse.equals("FILE_TOO_LARGE")){
-                                    Main.myLog("[Uploader] File too large");
-                                    dialog.fileTooLarge();  
-                                } else if(srvResponse.equals("SERVER_FULL")){
-                                    Main.myLog("[Uploader] Server Full");
-                                    dialog.serverFull();
-                                }
-                                
-                                progressDialog.setWait();
-                                break;
-                                
-                                // default case, hmm
-                            default:
-                                // TODO
-                                break;
-                        }
-                        
-                        // return link
-                        Main.myLog("[Uploader] Waiting link...");
-                        this.link = dis.readUTF();
-                        Main.myLog("[Uploader] Returned link: " + link);
-                        if (type.equals("file") || type.equals("txt"))
-                            progressDialog.close();
-                    bytes = null;
+					Main.myLog("Il server riceve un: " + type);
+
+					File file = new File(filePath);
+					long fileLength = file.length();
+					Main.myLog("[Uploader] File length: " + fileLength);
+					dos.writeLong(fileLength);
+					srvResponse = dis.readUTF();
+					if (srvResponse.equals("START_TRANSFER")) {
+						sendFile(file, fileLength);
+
+					} else if (srvResponse.equals("FILE_TOO_LARGE")) {
+						Main.myLog("[Uploader] File too large");
+						dialog.fileTooLarge();
+
+					} else if (srvResponse.equals("SERVER_FULL")) {
+						Main.myLog("[Uploader] Server Full");
+						dialog.serverFull();
+					}
+
+					progressDialog.setWait();
+
+					// return link
+					Main.myLog("[Uploader] Waiting link...");
+					this.link = dis.readUTF();
+					Main.myLog("[Uploader] Returned link: " + link);
+					progressDialog.close();
 				} else {
 					Main.myLog("[Uploader] The server had a bad interpretation of the fileType");
 					return false;
@@ -204,11 +108,11 @@ public class Uploader {
 	public void sendFile(File file, long fileLength) {
 		RandomAccessFile aFile = null;
 		try {
-			
+
 			aFile = new RandomAccessFile(file, "r");
 			final FileChannel inChannel = aFile.getChannel();
 
-			long bytesSent = 0; 
+			long bytesSent = 0;
 
 			progressDialog = new ProgressDialog();
 			progressDialog.setUploader(this);
@@ -216,24 +120,29 @@ public class Uploader {
 
 			// send the file
 			long bfSize = Math.min(131072, fileLength); // 128kB buffer
-            
-            while (bytesSent < fileLength) {
-                bytesSent += inChannel.transferTo(bytesSent, bfSize, socketChannel);
-                
-                // To secure overflow
-                Main.myLog("[Uploader] Sent: " + 100 * bytesSent / fileLength + "%");
-                progressDialog.set((int) (100 * bytesSent / fileLength));
-            }
+			while (bytesSent < fileLength) {
+				bytesSent += inChannel.transferTo(bytesSent, bfSize, socketChannel);
+
+				// To secure overflow
+				try {
+					Main.myLog("[Uploader] Sent: " + 100 * bytesSent / fileLength + "%");
+					progressDialog.set((int) (100 * bytesSent / fileLength));
+				} catch (Exception e) {
+					e.printStackTrace();
+					Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+				}
+			}
 			progressDialog.setWait();
 			inChannel.close();
 
+			Thread.sleep(1000);
 			Main.myLog("[Uploader] End of file reached..");
 			aFile.close();
 			Main.myLog("[Uploader] File closed.");
 
-		} catch (IOException e) {
+		} catch (IOException | InterruptedException e) {
 			e.printStackTrace();
-            Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+			Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
 		}
 
 	}
@@ -250,7 +159,7 @@ public class Uploader {
 			new File(new Environment().getTempDir(), "KStemp.zip").delete();
 		} catch (IOException e) {
 			e.printStackTrace();
-            Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+			Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
 		}
 	}
 
@@ -265,8 +174,8 @@ public class Uploader {
 
 		} catch (IOException | UnresolvedAddressException e) {
 			e.printStackTrace();
-            dialog.connectionError();
-            Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+			dialog.connectionError();
+			Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
 		}
 		return socketChannel;
 	}
