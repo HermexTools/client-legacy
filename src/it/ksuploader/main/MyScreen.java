518e2f52acd16e8446ed523e76d190921b04fe5a
SergiX44 <sergio.bri@outlook.it>
2015-06-19 19:45:41 +0200

    fix & revert

--- src/it/ksuploader/main/MyScreen.java
+++ src/it/ksuploader/main/MyScreen.java
@@ -116,18 +116,19 @@ public class MyScreen extends JPanel implements NativeKeyListener {
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		((Graphics2D) g).setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
-		g.setColor(c);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+		g2d.setColor(c);
 
 		Area fill = new Area(new Rectangle(new Point(0, 0), this.getSize()));
 		fill.subtract(new Area(selectionBounds));
+		g2d.fill(fill);
+		g2d.setColor(Color.RED);
+		g2d.draw(selectionBounds);
 
-		((Graphics2D) g).fill(fill);
-		g.setColor(Color.RED);
-		((Graphics2D) g).draw(fill);//draw(selectionBounds);
-
-        fill = null;
+		fill.reset();
+		g2d.dispose();
 		g.dispose();
 	}
 
