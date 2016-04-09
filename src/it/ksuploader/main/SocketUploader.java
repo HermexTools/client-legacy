8457ead7aa48987a48e46fb8fe1fb1a872afa8ac
Sergio Brighenti <sergio@brighenti.me>
2016-04-09 21:10:10 +0200

    updated socketuploader for the new server release

--- src/it/ksuploader/main/SocketUploader.java
+++ src/it/ksuploader/main/SocketUploader.java
@@ -1,5 +1,6 @@
 package it.ksuploader.main;
 
+
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -11,154 +12,151 @@ import java.util.Observable;
 import java.util.Observer;
 
 public class SocketUploader implements Observer {
-    private SocketChannel socketChannel;
-    private String link;
-    private String filePath;
-    private DataOutputStream dos;
-    private DataInputStream dis;
-    private RandomAccessFile aFile;
-    private FileChannel inChannel;
+	private SocketChannel socketChannel;
+	private String link;
+	private String filePath;
+	private DataOutputStream dos;
+	private DataInputStream dis;
+	private RandomAccessFile aFile;
+	private FileChannel inChannel;
 
-    public SocketUploader() {
-    }
+	private enum Messages {
+		OK,
+		BAD_SYN_STRING,
+		WRONG_PASSWORD,
+		FILE_NOT_RECOGNIZED,
+		FILE_TOO_LARGE,
+		SERVER_FULL,
+		UNKNOWN_ERROR
+	}
 
-    public boolean send(String type) {
-        try {
-            this.socketChannel = createChannel(Main.config.getIp(), Main.config.getPort());
-            this.dos = new DataOutputStream(socketChannel != null ? socketChannel.socket().getOutputStream() : null);
-            this.dis = new DataInputStream(socketChannel.socket().getInputStream());
+	public boolean send(String type) {
+		try {
+			this.socketChannel = createChannel(Main.config.getIp(), Main.config.getPort());
+			this.dos = new DataOutputStream(socketChannel != null ? socketChannel.socket().getOutputStream() : null);
+			this.dis = new DataInputStream(socketChannel.socket().getInputStream());
 
-            // send auth
-            Main.myLog("[SocketUploader] Sending auth");
-            dos.writeUTF(Main.config.getPass());
-            Main.myLog("[SocketUploader] Auth sent: " + Main.config.getPass());
-            this.link = dis.readUTF();
-            Main.myLog("[SocketUploader] Auth reply: " + link);
+			// send auth
+			Main.myLog("[SocketUploader] Sending SYN");
+			aFile = new RandomAccessFile(filePath, "r");
+			long fileLength = aFile.length();
 
-            if (this.link.equals("OK")) {
+			dos.writeUTF(Main.config.getPass() + "&" + fileLength + "&" + type);
+			this.link = dis.readUTF();
+			Main.myLog("[SocketUploader] SYN reply: " + link);
 
-                Main.myLog("Sending type: " + type);
-                dos.writeUTF(type);
-                String srvResponse;
+			switch (Messages.valueOf(this.link)) {
+				case OK:
+					inChannel = aFile.getChannel();
 
-                Main.myLog("Il server riceve un: " + type);
+					long bytesSent = 0;
+					long bfSize = Math.min(4096, fileLength);
+					Main.myLog("Transfer started.");
 
-                File file = new File(filePath);
-                long fileLength = file.length();
-                Main.myLog("[SocketUploader] File length: " + fileLength);
-                dos.writeLong(fileLength);
-                srvResponse = dis.readUTF();
-                switch (srvResponse) {
-                    case "START_TRANSFER":
+					Main.dialog.show("Uploading...", "", false);
+					Main.dialog.set(0);
 
-                        aFile = new RandomAccessFile(file, "r");
-                        inChannel = aFile.getChannel();
+					while (bytesSent < fileLength) {
+						bytesSent += inChannel.transferTo(bytesSent, bfSize, socketChannel);
+						Main.dialog.set((int) (100 * bytesSent / fileLength));
+					}
+					Main.myLog("Transfer ended.");
+					inChannel.close();
+					aFile.close();
+					Main.myLog("[SocketUploader] File closed.");
 
-                        long bytesSent = 0;
-                        long bfSize = Math.min(4096, fileLength);
-                        Main.myLog("Transfer started.");
+					Main.dialog.setWait();
+					break;
+				case BAD_SYN_STRING:
+					Main.dialog.show("Fatal Error!", "Bad SYN string", false);
+					Main.myLog("[SocketUploader] Bad SYN string");
+					break;
+				case FILE_TOO_LARGE:
+					Main.myLog("[SocketUploader] File too large");
+					Main.dialog.fileTooLarge();
+					break;
+				case SERVER_FULL:
+					Main.myLog("[SocketUploader] Server Full");
+					Main.dialog.serverFull();
+					break;
+				case FILE_NOT_RECOGNIZED:
+					Main.myLog("[SocketUploader] The server had a bad interpretation of the fileType");
+					Main.dialog.serverFull();
+					break;
+				case WRONG_PASSWORD:
+					Main.myLog("[SocketUploader] Wrong password, closed");
+					Main.dialog.wrongPassword();
+					return false;
+				case UNKNOWN_ERROR:
+					Main.dialog.show("Unknown Error!", "???", false);
+					Main.myLog("[SocketUploader] Unknown Error!");
+					break;
+			}
+			// return link
+			Main.myLog("[SocketUploader] Waiting link...");
+			this.link = dis.readUTF();
+			Main.myLog("[SocketUploader] Returned link: " + link);
+			Main.dialog.destroy();
 
-                        Main.dialog.show("Uploading...", "", false);
-                        Main.dialog.set(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+			return false;
+		} finally {
+			try {
+				inChannel.close();
+				aFile.close();
+				dis.close();
+				dos.flush();
+				dos.close();
+				socketChannel.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return true;
+	}
 
-                        while (bytesSent < fileLength) {
-                            bytesSent += inChannel.transferTo(bytesSent, bfSize, socketChannel);
-                            Main.dialog.set((int) (100 * bytesSent / fileLength));
-                        }
-                        Main.myLog("Transfer ended.");
-                        inChannel.close();
+	public void stopUpload() {
+		try {
+			inChannel.close();
+			aFile.close();
+			dos.close();
+			dis.close();
+			socketChannel.close();
+			new File(Main.so.getTempDir(), "KStemp.zip").delete();
+			Main.dialog.show("Stopped...", "", false);
+		} catch (IOException e) {
+			e.printStackTrace();
+			Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+		}
+	}
 
-                        Main.myLog("[SocketUploader] End of file reached..");
-                        aFile.close();
-                        Main.myLog("[SocketUploader] File closed.");
+	private SocketChannel createChannel(String ip, int port) {
+		try {
+			SocketChannel sc = SocketChannel.open();
+			SocketAddress socketAddress = new InetSocketAddress(ip, port);
+			sc.connect(socketAddress);
+			Main.myLog("[SocketUploader] Reloaded socket");
+			return sc;
+		} catch (IOException | UnresolvedAddressException e) {
+			e.printStackTrace();
+			Main.dialog.connectionError();
+			Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
+			return null;
+		}
+	}
 
-                        Main.dialog.setWait();
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
 
-                        break;
-                    case "FILE_TOO_LARGE":
-                        Main.myLog("[SocketUploader] File too large");
-                        Main.dialog.fileTooLarge();
+	public String getLink() {
+		return link;
+	}
 
-                        break;
-                    case "SERVER_FULL":
-                        Main.myLog("[SocketUploader] Server Full");
-                        Main.dialog.serverFull();
-                        break;
-
-                    case "FILE_NOT_RECOGNIZED":
-                        Main.myLog("[SocketUploader] The server had a bad interpretation of the fileType");
-                        Main.dialog.serverFull();
-                        break;
-                }
-                // return link
-                Main.myLog("[SocketUploader] Waiting link...");
-                this.link = dis.readUTF();
-                Main.myLog("[SocketUploader] Returned link: " + link);
-                Main.dialog.destroy();
-            } else {
-                Main.myLog("[SocketUploader] Wrong password, closed");
-                Main.dialog.wrongPassword();
-                return false;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-            Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
-            return false;
-        } finally {
-            try {
-                inChannel.close();
-                aFile.close();
-                dis.close();
-                dos.flush();
-                dos.close();
-                socketChannel.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return true;
-    }
-
-    public void stopUpload() {
-        try {
-            inChannel.close();
-            aFile.close();
-            dos.close();
-            dis.close();
-            socketChannel.close();
-            new File(Main.so.getTempDir(), "KStemp.zip").delete();
-            Main.dialog.show("Stopped...", "", false);
-        } catch (IOException e) {
-            e.printStackTrace();
-            Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
-        }
-    }
-
-    private SocketChannel createChannel(String ip, int port) {
-        try {
-            SocketChannel sc = SocketChannel.open();
-            SocketAddress socketAddress = new InetSocketAddress(ip, port);
-            sc.connect(socketAddress);
-            Main.myLog("[SocketUploader] Reloaded socket");
-            return sc;
-        } catch (IOException | UnresolvedAddressException e) {
-            e.printStackTrace();
-            Main.dialog.connectionError();
-            Main.myErr(Arrays.toString(e.getStackTrace()).replace(",", "\n"));
-            return null;
-        }
-    }
-
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
-
-    public String getLink() {
-        return link;
-    }
-
-    @Override
-    public void update(Observable o, Object arg) {
-        stopUpload();
-    }
+	@Override
+	public void update(Observable o, Object arg) {
+		stopUpload();
+	}
 }
