2ea1ae8549df0c2a0c133e82666cc334c2df012a
Luca Patera <lucapatera@outlook.it>
2016-09-19 23:52:13 +0200

    Fixed visualstudio's refactoring casino

--- KSLUploader-Client/KSLUploader-Client/Properties/Resources.Designer.cs
+++ KSLUploader-Client/KSLUploader-Client/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KSLUploaderuuuuuu.Properties {
+namespace KSLUploader.Properties {
     using System;
     
     
--- KSLUploader-Client/KSLUploader-Client/Properties/Settings.Designer.cs
+++ KSLUploader-Client/KSLUploader-Client/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace KSLUploaderuuuuuu.Properties {
+namespace KSLUploader.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
